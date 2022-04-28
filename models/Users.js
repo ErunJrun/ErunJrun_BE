05db9c1974e5df8313ad6d5952e6c070996aecc4
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-04-28 21:55:40 +0900

    [build] model 수정

--- models/Users.js
+++ models/Users.js
@@ -1,64 +1,68 @@
-const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('Users', {
-    userId: {
-      type: DataTypes.STRING(150),
-      allowNull: false,
-      primaryKey: true
-    },
-    nickname: {
-      type: DataTypes.STRING(150),
-      allowNull: false
-    },
-    bio: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    profileUrl: {
-      type: DataTypes.STRING(200),
-      allowNull: false
-    },
-    likeLocation: {
-      type: DataTypes.STRING(100),
-      allowNull: true
-    },
-    likeDistance: {
-      type: DataTypes.FLOAT,
-      allowNull: true
-    },
-    likeSpeed: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    mannerPoint: {
-      type: DataTypes.FLOAT,
-      allowNull: true
-    },
-    social: {
-      type: DataTypes.STRING(100),
-      allowNull: true
-    },
-    socialId: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    phone: {
-      type: DataTypes.STRING(100),
-      allowNull: true
-    }
-  }, {
-    sequelize,
-    tableName: 'Users',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "userId" },
-        ]
-      },
-    ]
-  });
-};
+const Sequelize = require('sequelize')
+const { UUIDV4 } = require('sequelize')
+module.exports = function (sequelize, DataTypes) {
+    return sequelize.define(
+        'Users',
+        {
+            userId: {
+                type: DataTypes.STRING(150),
+                defaultValue: UUIDV4,
+                allowNull: false,
+                primaryKey: true,
+            },
+            nickname: {
+                type: DataTypes.STRING(150),
+                allowNull: false,
+            },
+            bio: {
+                type: DataTypes.STRING(255),
+                allowNull: true,
+            },
+            profileUrl: {
+                type: DataTypes.STRING(200),
+                allowNull: false,
+            },
+            likeLocation: {
+                type: DataTypes.STRING(100),
+                allowNull: true,
+            },
+            likeDistance: {
+                type: DataTypes.FLOAT,
+                allowNull: true,
+            },
+            likeSpeed: {
+                type: DataTypes.INTEGER,
+                allowNull: true,
+            },
+            mannerPoint: {
+                type: DataTypes.FLOAT,
+                allowNull: true,
+            },
+            social: {
+                type: DataTypes.STRING(100),
+                allowNull: true,
+            },
+            socialId: {
+                type: DataTypes.STRING(255),
+                allowNull: true,
+            },
+            phone: {
+                type: DataTypes.STRING(100),
+                allowNull: true,
+            },
+        },
+        {
+            sequelize,
+            tableName: 'Users',
+            timestamps: false,
+            indexes: [
+                {
+                    name: 'PRIMARY',
+                    unique: true,
+                    using: 'BTREE',
+                    fields: [{ name: 'userId' }],
+                },
+            ],
+        }
+    )
+}
