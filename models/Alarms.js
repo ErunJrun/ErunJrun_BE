319cff7a3366c8bc8d8ab7315d58ce29c78f8e69
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-04-28 18:16:11 +0900

    [build] 최초배포

--- models/Alarms.js
+++ models/Alarms.js
@@ -1,69 +1,67 @@
-const Sequelize = require('sequelize')
-const { UUIDV4 } = require('sequelize')
-module.exports = function (sequelize, DataTypes) {
-    return sequelize.define(
-        'Alarms',
-        {
-            alarmId: {
-                type: DataTypes.STRING(150),
-                defaultValue: UUIDV4,
-                allowNull: false,
-                primaryKey: true,
-            },
-            userId: {
-                type: DataTypes.STRING(150),
-                allowNull: false,
-                references: {
-                    model: 'Users',
-                    key: 'userId',
-                },
-            },
-            category: {
-                type: DataTypes.STRING(150),
-                allowNull: false,
-            },
-            courseId: {
-                type: DataTypes.STRING(150),
-                allowNull: true,
-            },
-            courseTitle: {
-                type: DataTypes.STRING(150),
-                allowNull: true,
-            },
-            groupId: {
-                type: DataTypes.STRING(150),
-                allowNull: true,
-            },
-            groupTitle: {
-                type: DataTypes.STRING(150),
-                allowNull: true,
-            },
-            nickname: {
-                type: DataTypes.STRING(150),
-                allowNull: true,
-            },
-            role: {
-                type: DataTypes.STRING(100),
-                allowNull: true,
-            },
-        },
-        {
-            sequelize,
-            tableName: 'Alarms',
-            timestamps: true,
-            indexes: [
-                {
-                    name: 'PRIMARY',
-                    unique: true,
-                    using: 'BTREE',
-                    fields: [{ name: 'alarmId' }],
-                },
-                {
-                    name: 'FK_Users_TO_Alarms_1',
-                    using: 'BTREE',
-                    fields: [{ name: 'userId' }],
-                },
-            ],
-        }
-    )
-}
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('Alarms', {
+    alarmId: {
+      type: DataTypes.STRING(150),
+      allowNull: false,
+      primaryKey: true
+    },
+    userId: {
+      type: DataTypes.STRING(150),
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'userId'
+      }
+    },
+    category: {
+      type: DataTypes.STRING(150),
+      allowNull: false
+    },
+    courseId: {
+      type: DataTypes.STRING(150),
+      allowNull: true
+    },
+    courseTitle: {
+      type: DataTypes.STRING(150),
+      allowNull: true
+    },
+    groupId: {
+      type: DataTypes.STRING(150),
+      allowNull: true
+    },
+    groupTitle: {
+      type: DataTypes.STRING(150),
+      allowNull: true
+    },
+    nickname: {
+      type: DataTypes.STRING(150),
+      allowNull: true
+    },
+    role: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    tableName: 'Alarms',
+    timestamps: true,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "alarmId" },
+        ]
+      },
+      {
+        name: "FK_Users_TO_Alarms_1",
+        using: "BTREE",
+        fields: [
+          { name: "userId" },
+        ]
+      },
+    ]
+  });
+};
