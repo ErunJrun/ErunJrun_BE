e2a5950828283be2f8627b1256be5d7addd1b33f
DongYoung-dev <86117661+DongYoung-dev@users.noreply.github.com>
2022-04-29 18:56:12 +0900

    [fix] 그룹러닝 개설자는 신청을 취소하지 못하도록 수정

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use((req, res, next) => {
     res.status(404).send('요청하신 페이지를 찾을 수 없습니다')
 })
 
+
+
 app.use((err, req, res, next) => {
     res.json({ success: false, message: err.message })
 })
