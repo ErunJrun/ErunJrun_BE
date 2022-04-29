4ea938f321d559b5bbcf2131e42d5c596b666058
inmyblue <98289614+inmyblue@users.noreply.github.com>
2022-04-29 11:26:44 +0900

    [feat] 그룹러닝 게시물 불러오기 API 추가

--- groups/services/group.service.js
+++ groups/services/group.service.js
@@ -1,20 +1,65 @@
 const sequelize = require('sequelize')
-const { Groups } = require('../../models')
+const { Groups, Appliers, Users } = require('../../models')
 
 module.exports = {
     createPost: async (data) => {
         await Groups.create(data)
         return
     },
-    getGroupData: (limit) => {
-        const data = Groups.findAll({
-            limit,
+    getGroupData: async (limit, myUserId) => {
+        let condition = {}
+        if (myUserId) condition = { userId: myUserId }
+
+        let data = await Groups.findAll({
+            where: condition,
+            attributes: [
+                'title',
+                'location',
+                'distance',
+                'groupId',
+                'date',
+                'startTime',
+                'maxPeople',
+                ['thumbnailUrl1', 'thumbnailUrl'],
+                [
+                    sequelize.fn('COUNT', sequelize.col('Appliers.applyId')),
+                    'applyPeople',
+                ],
+                'userId',
+            ],
+            include: [
+                {
+                    model: Appliers,
+                    as: 'Appliers',
+                    foreignKey: 'userId',
+                    attributes: [],
+                },
+            ],
+            group: ['groupId'],
+            order: [['createdAt', 'DESC']],
+            // limit,
+        }).then(async (result) => {
+            for (let i = 0; i < result.length; i++) {
+                const user = await Users.findOne({
+                    where: {
+                        userId: result[i].userId,
+                    },
+                })
+
+                result[i].dataValues.nickname = user.nickname
+                result[i].dataValues.profileUrl = user.profileUrl
+            }
+            return result
         })
+        if (limit) {
+            data = data.slice(0, 3)
+        }
         return data
     },
-    getMyGroupData: async (userId) => {
-        const data = await Groups.findAll({
+    getMyGroupData: (userId) => {
+        const data = Groups.findAll({
             where: { userId },
+            order: [['createdAt', 'DESC']],
         })
         return data
     },
