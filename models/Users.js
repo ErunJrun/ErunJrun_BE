62a71bd71c511636172d6ed2990f993030e1216f
inmyblue <98289614+inmyblue@users.noreply.github.com>
2022-04-29 20:52:27 +0900

    [build] User 모델 데이터형식변경(선호거리, 선호속도)

--- models/Users.js
+++ models/Users.js
@@ -27,11 +27,11 @@ module.exports = function (sequelize, DataTypes) {
                 allowNull: true,
             },
             likeDistance: {
-                type: DataTypes.FLOAT,
+                type: DataTypes.STRING(50),
                 allowNull: true,
             },
             likeSpeed: {
-                type: DataTypes.INTEGER,
+                type: DataTypes.STRING(50),
                 allowNull: true,
             },
             mannerPoint: {
