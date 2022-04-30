9c240581210e60271d6b84aec668bd1a3842fcca
inmyblue <98289614+inmyblue@users.noreply.github.com>
2022-04-30 13:50:08 +0900

    [fix] helmet 임시해제(개발단계)

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 require('express-async-errors')
 
 app.use(cors())
-app.use(helmet())
+// app.use(helmet())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.static('public'))
@@ -21,8 +21,6 @@ app.use('/', Router)
 app.use((req, res, next) => {
     res.status(404).send('요청하신 페이지를 찾을 수 없습니다')
 })
-
-
 
 app.use((err, req, res, next) => {
     res.json({ success: false, message: err.message })
