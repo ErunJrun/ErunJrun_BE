9e5575591510146c44567f3ff72697a33d866569
inmyblue <98289614+inmyblue@users.noreply.github.com>
2022-04-30 17:28:20 +0900

    [fix] 폼데이터 배열 찾기(프론트 테스트용)

--- groups/controllers/group.controller.js
+++ groups/controllers/group.controller.js
@@ -21,7 +21,7 @@ module.exports = {
             content: req.body.content,
             mapLatLng: req.body.mapLatLng,
         }
-        console.log(JSON.stringify(req.body.mapLatLng))
+        console.log(JSON.stringify(req.body.mapLatLng[0]))
 
         try {
             if (req.files) {
