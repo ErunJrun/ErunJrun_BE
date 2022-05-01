c9a1266a7443edecb06647382e260d1287c82c00
inmyblue <98289614+inmyblue@users.noreply.github.com>
2022-05-01 09:08:57 +0900

    [fix] 그룹러닝 update시에도 지역/시간필터 추가하도록 수정

--- groups/services/group.service.js
+++ groups/services/group.service.js
@@ -71,7 +71,6 @@ module.exports = {
                 limit = 3
                 break
             case 'all':
-                //날짜필터는 필수값
                 let distanceCondition
                 let dateCondition
                 let timeCondition
@@ -264,6 +263,50 @@ module.exports = {
         return data
     },
     updatePost: (groupId, data) => {
+        const regionText = data.location.split(' ')[0]
+        let region
+        switch (regionText) {
+            case '서울':
+                region = 1
+                break
+            case '경기':
+                region = 2
+                break
+            case '인천':
+                region = 3
+                break
+            case '강원':
+                region = 4
+                break
+            case '충남':
+            case '충북':
+            case '세종특별자치시':
+                region = 5
+                break
+            case '경북':
+            case '대구':
+                region = 6
+                break
+            case '경남':
+            case '부산':
+            case '울산':
+                region = 7
+                break
+            case '전남':
+            case '전북':
+            case '광주':
+                region = 8
+                break
+            case '제주특별자치도':
+                region = 9
+                break
+        }
+
+        let timeCode = data.startTime.split(':')[0]
+        timeCode = Math.ceil(timeCode / 4)
+
+        data.region = region
+        data.timecode = timeCode
         Groups.update(data, { where: { groupId } })
         return
     },
