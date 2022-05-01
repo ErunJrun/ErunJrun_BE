95a4e1116a2901d4731de4f6eef398993fcb3f8d
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-05-02 00:05:11 +0900

    [doc] development pull받고 다시 push}

--- auth/index.js
+++ auth/index.js
@@ -2,5 +2,6 @@ const express = require('express')
 const router = express.Router()
 const authController = require('./controllers/auth.controller')
 
+
 router.get('/info/:userId', authController.getUserInfo)
 module.exports = router
