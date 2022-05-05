a9b49909deb647072fbff01698e845091c627f69
DongYoung-dev <86117661+DongYoung-dev@users.noreply.github.com>
2022-05-05 16:01:21 +0900

    [feat] auth 라우터 멀터추가

--- auth/index.js
+++ auth/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('./controllers/auth.controller')
+const multer = require('../middlewares/multers/multer')
 
 router.get('/info/:userId', authController.getUserInfo)
 router.patch('info/like', authController.applyUserLike)
