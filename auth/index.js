e83e034ad64c08d634dbe29e99e4b60550b8ce6a
inmyblue <98289614+inmyblue@users.noreply.github.com>
2022-05-05 21:29:49 +0900

    최신화

--- auth/index.js
+++ auth/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('./controllers/auth.controller')
+const multer = require('../middlewares/multers/multer')
 
 router.get('/info/:userId', authController.getUserInfo)
-router.patch('info/like', authController.applyUserLike)
+router.patch('/userLike', authController.applyUserLike)
+router.patch('/updateUser', multer.uploadProfile.single('image'), authController.updateUserInfo)
+
 module.exports = router
