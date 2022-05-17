1b2735d347a7ed870bdd0bf7872198df16a48ff9
DongYoung-dev <86117661+DongYoung-dev@users.noreply.github.com>
2022-05-17 16:17:33 +0900

    [fix] 소셜로그인시 닉네임 8자이상 자르기

--- passport/kakaoStrategy.js
+++ passport/kakaoStrategy.js
@@ -27,8 +27,9 @@ module.exports = () => {
                     if (exUser) {
                         done(null, exUser) // 로그인 인증 완료
                     } else {
+                        let nickname = ''
                         if(profile._json.properties.nickname.length > 8) {
-                            let nickname = profile._json.properties.nickname.substr(0, 8)
+                            nickname = profile._json.properties.nickname.substr(0, 8)
                         }
                         // 가입되지 않는 유저면 회원가입 시키고 로그인을 시킨다
                         const newUser = await Users.create({
