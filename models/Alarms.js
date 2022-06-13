1a0e5d8c1913e157bfd44f2118b322f32f448420
DongYoung-dev <86117661+DongYoung-dev@users.noreply.github.com>
2022-06-13 11:43:33 +0900

    [fix] 채팅기능 수정

--- models/Alarms.js
+++ models/Alarms.js
@@ -1,81 +1,81 @@
-const Sequelize = require('sequelize')
-module.exports = function (sequelize, DataTypes) {
-    return sequelize.define(
-        'Alarms',
-        {
-            alarmId: {
-                type: DataTypes.STRING(150),
-                allowNull: false,
-                primaryKey: true,
-            },
-            userId: {
-                type: DataTypes.STRING(150),
-                allowNull: false,
-                references: {
-                    model: 'Users',
-                    key: 'userId',
-                },
-            },
-            category: {
-                type: DataTypes.STRING(150),
-                allowNull: false,
-            },
-            courseId: {
-                type: DataTypes.STRING(150),
-                allowNull: true,
-            },
-            courseTitle: {
-                type: DataTypes.STRING(150),
-                allowNull: true,
-            },
-            groupId: {
-                type: DataTypes.STRING(150),
-                allowNull: true,
-            },
-            groupTitle: {
-                type: DataTypes.STRING(150),
-                allowNull: true,
-            },
-            nickname: {
-                type: DataTypes.STRING(150),
-                allowNull: true,
-            },
-            role: {
-                type: DataTypes.STRING(100),
-                allowNull: true,
-            },
-            check: {
-                type: DataTypes.BOOLEAN,
-                allowNull: false,
-                defaultValue: 0,
-            },
-            commentId: {
-                type: DataTypes.STRING(150),
-                allowNull: true,
-            },
-            sendPhone: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                defaultValue: 0,
-            },
-        },
-        {
-            sequelize,
-            tableName: 'Alarms',
-            timestamps: true,
-            indexes: [
-                {
-                    name: 'PRIMARY',
-                    unique: true,
-                    using: 'BTREE',
-                    fields: [{ name: 'alarmId' }],
-                },
-                {
-                    name: 'FK_Users_TO_Alarms_1',
-                    using: 'BTREE',
-                    fields: [{ name: 'userId' }],
-                },
-            ],
-        }
-    )
-}
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('Alarms', {
+    alarmId: {
+      type: DataTypes.STRING(150),
+      allowNull: false,
+      primaryKey: true
+    },
+    userId: {
+      type: DataTypes.STRING(150),
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'userId'
+      }
+    },
+    category: {
+      type: DataTypes.STRING(150),
+      allowNull: false
+    },
+    courseId: {
+      type: DataTypes.STRING(150),
+      allowNull: true
+    },
+    courseTitle: {
+      type: DataTypes.STRING(150),
+      allowNull: true
+    },
+    groupId: {
+      type: DataTypes.STRING(150),
+      allowNull: true
+    },
+    groupTitle: {
+      type: DataTypes.STRING(150),
+      allowNull: true
+    },
+    nickname: {
+      type: DataTypes.STRING(150),
+      allowNull: true
+    },
+    role: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
+    check: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: 0
+    },
+    commentId: {
+      type: DataTypes.STRING(150),
+      allowNull: true
+    },
+    sendPhone: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    }
+  }, {
+    sequelize,
+    tableName: 'Alarms',
+    timestamps: true,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "alarmId" },
+        ]
+      },
+      {
+        name: "FK_Users_TO_Alarms_1",
+        using: "BTREE",
+        fields: [
+          { name: "userId" },
+        ]
+      },
+    ]
+  });
+};
--- models/Users.js
+++ models/Users.js
@@ -1,72 +1,70 @@
-const Sequelize = require('sequelize')
-module.exports = function (sequelize, DataTypes) {
-    return sequelize.define(
-        'Users',
-        {
-            userId: {
-                type: DataTypes.STRING(150),
-                allowNull: false,
-                primaryKey: true,
-            },
-            nickname: {
-                type: DataTypes.STRING(150),
-                allowNull: false,
-            },
-            bio: {
-                type: DataTypes.STRING(255),
-                allowNull: true,
-            },
-            profileUrl: {
-                type: DataTypes.STRING(200),
-                allowNull: false,
-            },
-            likeLocation: {
-                type: DataTypes.STRING(100),
-                allowNull: true,
-            },
-            likeDistance: {
-                type: DataTypes.STRING(50),
-                allowNull: true,
-            },
-            userLevel: {
-                type: DataTypes.STRING(50),
-                allowNull: true,
-            },
-            mannerPoint: {
-                type: DataTypes.FLOAT,
-                allowNull: false,
-                defaultValue: 50,
-            },
-            social: {
-                type: DataTypes.STRING(100),
-                allowNull: true,
-            },
-            socialId: {
-                type: DataTypes.STRING(255),
-                allowNull: true,
-            },
-            phone: {
-                type: DataTypes.STRING(100),
-                allowNull: true,
-            },
-            agreeSMS: {
-                type: DataTypes.TINYINT,
-                allowNull: false,
-                defaultValue: 0,
-            },
-        },
-        {
-            sequelize,
-            tableName: 'Users',
-            timestamps: false,
-            indexes: [
-                {
-                    name: 'PRIMARY',
-                    unique: true,
-                    using: 'BTREE',
-                    fields: [{ name: 'userId' }],
-                },
-            ],
-        }
-    )
-}
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('Users', {
+    userId: {
+      type: DataTypes.STRING(150),
+      allowNull: false,
+      primaryKey: true
+    },
+    nickname: {
+      type: DataTypes.STRING(150),
+      allowNull: false
+    },
+    bio: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    profileUrl: {
+      type: DataTypes.STRING(200),
+      allowNull: false
+    },
+    likeLocation: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
+    likeDistance: {
+      type: DataTypes.STRING(50),
+      allowNull: true
+    },
+    userLevel: {
+      type: DataTypes.STRING(50),
+      allowNull: true
+    },
+    mannerPoint: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      defaultValue: 50
+    },
+    social: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
+    socialId: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    phone: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
+    agreeSMS: {
+      type: DataTypes.TINYINT,
+      allowNull: false,
+      defaultValue: 0
+    }
+  }, {
+    sequelize,
+    tableName: 'Users',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "userId" },
+        ]
+      },
+    ]
+  });
+};
